c14406cf78a7b0a70800f3923e17667e8dd54293
Abhay-Singh-00 <abhayrathore002>
2025-08-15 15:51:41 +0530

    updated path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   darkMode: 'class',
-  base: '/CARPUR/',
+  base: '/',
   plugins: [react(),tailwindcss()],
 })
